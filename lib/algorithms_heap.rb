f88255caf2918325d36de7af2bd60c4d2833f61d
Rathi <github email address>
2017-08-25 12:43:13 -0400

    included push method which pushes in value given and also tracks the min value

--- lib/algorithms_heap.rb
+++ lib/algorithms_heap.rb
@@ -1,8 +1,20 @@
 class MinHeap
+  attr_accessor :root
+
   def initialize
     @data = []
+    self.root = nil
   end
+
   def data
     @data
   end
+
+  def push(value)
+   @data.push(value)
+   if self.root == nil || self.root > value
+     self.root = value
+   end
+  end
+  
 end
