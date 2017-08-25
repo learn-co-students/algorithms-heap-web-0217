1f8a0a04ff7d507ae2b61851a15d14140010b81e
Rathi <github email address>
2017-08-25 18:29:10 -0400

    did the pop

--- lib/algorithms_heap.rb
+++ lib/algorithms_heap.rb
@@ -16,5 +16,18 @@ class MinHeap
      self.root = value
    end
   end
-  
+
+  def pop
+   popped = @data.delete_at(-1)
+   if root == popped && !data.include?(root)
+      min = @data[0]
+      @data[1..-1].each do |num|
+        if num < min
+          min = num
+        end
+      end
+      self.root = min
+    end
+    popped
+  end
 end
